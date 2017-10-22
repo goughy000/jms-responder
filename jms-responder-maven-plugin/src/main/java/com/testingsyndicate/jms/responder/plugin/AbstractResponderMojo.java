b953005963e0e816358a033e1ff3accaed8192f4
Jack Gough <goughy000@gmail.com>
2017-10-22 09:05:14 +0100

    Added maven plugin

---
+++ jms-responder-maven-plugin/src/main/java/com/testingsyndicate/jms/responder/plugin/AbstractResponderMojo.java
@@ -0,0 +1,9 @@
+package com.testingsyndicate.jms.responder.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+
+abstract class AbstractResponderMojo extends AbstractMojo {
+
+    static final String SERVER_KEY = "com.testingsyndicate.jms.responder.plugin.SERVER";
+
+}
